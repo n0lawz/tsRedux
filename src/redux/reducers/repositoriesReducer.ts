f15f73eab36688788cd7fda1484fb4fa28039f08
Nat <nlaw.dev@gmail.com>
2022-03-26 13:01:38 -0700

    updated reducer with ts, used enums for actiontype and created seperate interfaces for actions

--- src/redux/reducers/repositoriesReducer.ts
+++ src/redux/reducers/repositoriesReducer.ts
@@ -1,30 +1,45 @@
+// interface to describe the state 
 interface RepositoriesState {
     loading: boolean;
     error: string | null;
     data: string[];
 }
 
+// these are the seperate interfaces that describe each potential action
 interface SearchRepositoriesAction {
-    type: 'search_repositories';
+    type: ActionType.SEARCH_REPOSITORIES;
 }
 
 interface SearchRepositoriesSuccess {
-    type: 'search_repositories_success',
+    type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
     payload: string[];
 }
 
 interface SearchRepositoriesError {
-    type: 'search_repositories_error',
+    type: ActionType.SEARCH_REPOSITORIES_ERROR,
     payload: string;
 }
 
-const reducer = (state: RepositoriesState, action: SearchRepositoriesAction | SearchRepositoriesSuccess | SearchRepositoriesError ): RepositoriesState => {
+// type to pass into our reducer to describe our Actions
+type Action =
+    | SearchRepositoriesAction
+    | SearchRepositoriesSuccess 
+    | SearchRepositoriesError;
+
+// enum to hold action type strings so we can use in reducer and other interfaces
+enum ActionType {
+    SEARCH_REPOSITORIES ='search_repositories',
+    SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
+    SEARCH_REPOSITORIES_ERROR = 'search_repositories_error'
+}
+
+const reducer = (state: RepositoriesState, action: Action ): RepositoriesState => {
     switch (action.type) {
-        case 'search_repositories':
+        case ActionType.SEARCH_REPOSITORIES:
             return { loading: true, error: null, data: [] }
-        case 'search_repositories_success':
+        case ActionType.SEARCH_REPOSITORIES_SUCCESS:
             return { loading: false, error: null, data: action.payload }
-        case 'search_repositories_error':
+        case ActionType.SEARCH_REPOSITORIES_ERROR:
             return { loading: false, error: action.payload, data: [] }
 
         default:
