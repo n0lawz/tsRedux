ab3553f34f7459ef7a9845609dadb3d7985ac60b
Nat <nlaw.dev@gmail.com>
2022-03-28 11:00:18 -0700

    installed dispatch type definition

--- src/redux/action-creators/index.ts
+++ src/redux/action-creators/index.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
+import { Dispatch } from 'redux';
 import { ActionType } from '../action-types';
 import { Action } from '../actions';
 
 export const searchRepositories = (term: string) => {
-    return async (dispatch: any) => {
+    return async (dispatch: Dispatch<Action>) => {
         dispatch({
             type: ActionType.SEARCH_REPOSITORIES
         });
@@ -22,7 +23,7 @@ export const searchRepositories = (term: string) => {
             dispatch({
                 type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
                 payload: names
-            })
+            });
         } catch (err: any) {
             dispatch({
                 type: ActionType.SEARCH_REPOSITORIES_ERROR,
